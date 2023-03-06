4c5cf4a8bebb3d4f4ce0b737c8e6805546883757
nastiakostenyuk <nastuakostenyk@gmail.com>
2023-03-06 20:38:41 +0200

    add new urls

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from .views import *
+app_name = 'news'
 
 urlpatterns = [
-    path("news/", home, name='home')
+    path("news/", home, name='home'),
+    path('category/all', all_categories, name='all_categories'),
+    path('category/<int:category_id>', category, name='category'),
+    path('news/<int:news_id>', news_detail, name='news_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,9 +1,27 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
+from .models import News, Category
+
 
 def home(request):
-    return HttpResponse("Новини")
+    return HttpResponse("Сайт з новинами :)")
+
+
+def all_categories(request):
+    categories = Category.objects.all()
+    return render(request, 'news/all_category.html', {'categories': categories})
+
+
+def category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    news = News.objects.filter(categories=category)
+    return render(request, 'news/category.html', {'news': news, 'category': category})
+
+
+def news_detail(request, news_id):
+    news = News.objects.get(id=news_id)
+    return render(request, 'news/news_detail.html', {'news': news})
 
 
 def pageNotFound(request, exception):
